fff6d982e508a75f8add6ba05a4a9360a45fea28
Søren Lind Kristiansen <soren@gutsandglory.dk>
2017-10-23 21:21:57 +0200

    Refactor to make code more readable.

--- lemma/lemmatizer.py
+++ lemma/lemmatizer.py
@@ -1,12 +1,8 @@
 # coding: utf8
 """Functions for lemmatizing using a set of lemmatization rules."""
+from collections import defaultdict
 import logging
 import time
-
-# TODO: Eventually we want pos tags instead of word_class?
-# TODO: Normalize quotes (specifically single quotes: A'er)
-# TODO: Multi-word phrases?
-# TODO: Adjectives can also be adverbs?
 
 
 class Lemmatizer(object):  # pylint: disable=too-few-public-methods
@@ -18,13 +14,13 @@ class Lemmatizer(object):  # pylint: disable=too-few-public-methods
 
     def lemmatize(self, word_class, full_form):
         """Return lemma for specified full form word of specified word class."""
-        rule = _find_rule(self.rules, word_class, full_form)
+        rule = _longest_matching_rule(self.rules, word_class, full_form)
         predicted_lemma = _apply_rule(rule, full_form)
         return predicted_lemma
 
     def fit(self, X, y, max_iteration: int = 20):
         """Train a lemmatizer on specified training data."""
-        self.rules = {}
+        self.rules = defaultdict(lambda: defaultdict(lambda: []))
         old_rule_count = -1
         epoch = 1
         train_start = time.time()
@@ -44,47 +40,44 @@ class Lemmatizer(object):  # pylint: disable=too-few-public-methods
 
     def _train_epoch(self, X, y):
         for (word_class, full_form), lemma in zip(X, y):
-            rule = _find_rule(self.rules, word_class, full_form)
+            rule = _longest_matching_rule(self.rules, word_class, full_form)
             predicted_lemmas = _apply_rule(rule, full_form)
 
             if lemma in predicted_lemmas:
                 # Current rules yield the correct lemma, so nothing to do.
                 continue
 
-            # Current rules don't yield the correct lemma, so add a new rule.
-            min_rule_length = len(rule[0]) + 1
+            # Current rules don't yield the correct lemma, so we will add a new rule. To make sure the new rule will
+            # be used, try to make it at least one character longer than existing longest matching rule.
+            current_rule_length = len(rule[0])
+            full_form_suffix, lemma_suffix, exhausted = _create_rule(full_form, lemma, current_rule_length)
 
-            if min_rule_length >= len(full_form):
-                full_form_suffix, lemma_suffix = full_form, lemma
-                locked = True
+            if not exhausted:
+                # New rule does not exhaust full form, meaning we are not using the complete full form for suffix.
+                # Therefore it's safe to assume the new rule is longer than previous matching rule, and subsequently
+                # that no rules with the new suffix exist. And so, we don't have to consider existing rules.
+                self.rules[word_class][full_form_suffix] = [(lemma_suffix, exhausted)]
             else:
-                full_form_suffix, lemma_suffix = _build_rule(lemma, full_form, min_rule_length)
-                locked = len(full_form_suffix) == len(full_form)
+                # New rule exhausts full form, meaning we may or may not have an existing rule with the new suffix.
+                # If we have existing rules and they are locked, we will keep them. Otherwise we will clear existing
+                # rules (if any).
+                if not self._locked_rule_exists(word_class, full_form_suffix):
+                    self.rules[word_class][full_form_suffix] = []
+                self.rules[word_class][full_form_suffix] += [(lemma_suffix, True)]
 
-            if word_class not in self.rules:
-                self.rules[word_class] = {}
-            if full_form_suffix not in self.rules[word_class]:
-                self.rules[word_class][full_form_suffix] = []
-
-            if not self.rules[word_class][full_form_suffix]:
-                # no existing rules, so we can just add
-                self.rules[word_class][full_form_suffix] = [(lemma_suffix, locked)]
-            else:
-                # current must be locked because if it wasn't, the new rule would be longer than any existing matching
-                # rule
-                assert locked
-                existing_locked = self.rules[word_class][full_form_suffix][0][1]
-                if existing_locked:
-                    self.rules[word_class][full_form_suffix] += [(lemma_suffix, True)]
-                else:
-                    self.rules[word_class][full_form_suffix] = [(lemma_suffix, True)]
+    def _locked_rule_exists(self, word_class, full_form_suffix):
+        rules_list = self.rules[word_class][full_form_suffix]
+        if not rules_list:
+            return False
+        return rules_list[0][1]
 
     def _prune(self, X):
-        logging.debug("rules before pruning: %s", self._count_rules())
+        pre_prune_count = self._count_rules()
+        logging.debug("rules before pruning: %s", pre_prune_count)
         used_rules = {}
 
         for word_class, full_form in X:
-            full_form_suffix, lemmas = _find_rule(self.rules, word_class, full_form)
+            full_form_suffix, lemmas = _longest_matching_rule(self.rules, word_class, full_form)
             if full_form_suffix == "" and lemmas[0] == "":
                 continue
             used_rules[word_class + "_" + full_form_suffix] = len(lemmas)
@@ -97,29 +90,48 @@ class Lemmatizer(object):  # pylint: disable=too-few-public-methods
                 if word_class + "_" + full_form_suffix not in used_rules:
                     word_class_rules.pop(full_form_suffix)
 
-        logging.debug("rules after pruning: %s", self._count_rules())
+        post_prune_count = self._count_rules()
+        logging.debug("rules after pruning: %s (%s removed)", post_prune_count, pre_prune_count - post_prune_count)
 
 
-def _build_rule(lemma, full_form, min_rule_length):
-    assert min_rule_length < len(full_form)
-    if min_rule_length == len(full_form):
-        return full_form, lemma
-    max_length_in_common = len(full_form) - min_rule_length
-    length_in_common = min(len(lemma), max_length_in_common)
-    # TODO: Refactor, the 'i = i -i' is ugly.
-    i = 0
-    for i, char in enumerate(full_form[:length_in_common]):
-        if lemma[i] != char:
-            i = i - 1
-            break
+def _create_rule(full_form, lemma, current_rule_length):
+    if current_rule_length >= len(full_form) - 1:
+        # The current longest matching rule is at least as long as the full form minus one character. Thus, building
+        # a longer rule will use the entire full form.
+        full_form_suffix, lemma_suffix, exhausted = full_form, lemma, True
+        return full_form_suffix, lemma_suffix, exhausted
 
-    full_form_suffix = full_form[i + 1:]
-    lemma_suffix = lemma[i + 1:]
+    min_rule_length = current_rule_length + 1
+    split_position = _find_suffix_start(full_form, lemma, min_rule_length)
+    full_form_suffix = full_form[split_position:]
+    lemma_suffix = lemma[split_position:]
     assert min_rule_length <= len(full_form_suffix)
-    return full_form_suffix, lemma_suffix
+
+    exhausted = len(full_form_suffix) == len(full_form)
+    return full_form_suffix, lemma_suffix, exhausted
 
 
-def _find_rule(rules, word_class, full_form):
+def _find_suffix_start(full_form, lemma, min_rule_length):
+    """
+    Find and return the index at which the suffix begins.
+
+    Full form and lemma will have all characters to the left of the suffix in common. The split will be made far enough
+    to the left to allow for the suffix to consist of at least 'min_rule_length' characters.
+    """
+    max_prefix_length = _max_full_form_prefix_length(full_form, lemma, min_rule_length)
+    suffix_start = 0
+    while suffix_start < max_prefix_length and lemma[suffix_start] == full_form[suffix_start]:
+        suffix_start += 1
+    return suffix_start
+
+
+def _max_full_form_prefix_length(full_form, lemma, min_rule_length):
+    """Return the maximum possible length of the prefix (part of the full form preceding the suffix)."""
+    full_form_prefix = len(full_form) - min_rule_length
+    return min(len(lemma), full_form_prefix)
+
+
+def _longest_matching_rule(rules, word_class, full_form):
     """Find the rule with the longest full form suffix matching specified full form and class."""
     best = ("", [""])
     if word_class not in rules:
