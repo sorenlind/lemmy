c290ae7e3c08419536fee23ffb5fdacaf635e6f3
Søren Lind Kristiansen <soren@gutsandglory.dk>
2018-02-09 09:46:50 +0100

    Add spaCy pipeline component and example notebook

--- lemma/__init__.py
+++ lemma/__init__.py
@@ -1,3 +1,4 @@
 """Lemma."""
 from lemma.lemmatizer import Lemmatizer, load
 from lemma.simple_lemmatizer import SimpleLemmatizer
+from lemma.pipeline_component import load_pipeline_component
---
+++ lemma/pipeline_component.py
@@ -0,0 +1,52 @@
+# coding: utf8
+"""A spaCy pipeline component."""
+from lemma import Lemmatizer
+from lemma.rules import rules as default_rules
+from spacy.tokens import Token
+from spacy.symbols import PRON_LEMMA
+
+
+class LemmaPipelineComponent(object):
+    """
+    A pipeline component for spaCy.
+
+    This wraps a trained lemmatizer for easy use with spaCy.
+    """
+
+    name = 'lemma'
+
+    def __init__(self, rules):
+        """Initialize a pipeline component instance."""
+        self._internal = Lemmatizer(rules)
+        self._lemma = 'lemma'
+
+        # Add attributes
+        Token.set_extension(self._lemma, default=None)
+
+    def __call__(self, doc):
+        """
+        Apply the pipeline component to a `Doc` object.
+
+        doc (Doc): The `Doc` returned by the previous pipeline component.
+        RETURNS (Doc): The modified `Doc` object.
+        """
+        for token in doc:
+            if token.lemma_ == PRON_LEMMA:
+                lemma = PRON_LEMMA
+            else:
+                lemma = self._get_lemma(token)
+
+            if not lemma:
+                continue
+            token._.set(self._lemma, lemma)
+        return doc
+
+    def _get_lemma(self, token):
+        lemmas = self._internal.lemmatize(token.pos_, token.text)
+        if len(lemmas) != 1:
+            return None
+        return lemmas[0]
+
+
+def load_pipeline_component():
+    return LemmaPipelineComponent(default_rules)
